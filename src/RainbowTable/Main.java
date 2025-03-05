18c1155f20e1fe239b2133cf04ef5cc03f771da1
CristhianLeonMolina <cristhianleon@gmail.com>
2025-03-05 12:30:32 +0100

    updated

---
+++ src/RainbowTable/Main.java
@@ -0,0 +1,12 @@
+package RainbowTable;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+
+
+    }
+}
